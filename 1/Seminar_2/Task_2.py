8d2daf9e60c255965974dce5d489ff91691394fe
Victoriya <vikewa@mail.ru>
2023-09-13 11:37:22 +0300

    Решии 2ю задачу

---
+++ 1/Seminar_2/Task_2.py
@@ -0,0 +1,27 @@
+# Дано натуральное число A > 1. Определите, каким по счету числом Фибоначчи оно является, то есть выведите такое число n, что φ(n)=A. Если А не
+# является числом Фибоначчи, выведите число -1.
+# Input: 5
+ # Output: 6 
+
+
+num = input("Введите число: ")
+
+while not num.isdigit():
+    print("Вы ввели НЕ число.")
+    num = input("Введите целое число n: ")
+
+num = int(num)
+fib1 = 0
+fib2 = 1
+fib = 1
+count = 2
+
+while fib < num:
+   fib = fib1 + fib2
+   fib1 = fib2
+   fib2 = fib
+   count +=1
+if fib == num:
+    print(count)
+else:
+    print(-1)
